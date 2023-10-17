6432e5d90d9b9fcdb86d889d2aefa0bcd4a1c131
abdalrahemaldaker <102296021+abdalrahemaldaker@users.noreply.github.com>
2023-10-18 00:46:18 +0300

    admin_homepage

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -3,10 +3,15 @@
 namespace App\Admin\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Customer;
+use App\Models\Invoice;
 use Encore\Admin\Controllers\Dashboard;
 use Encore\Admin\Layout\Column;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Layout\Row;
+use Encore\Admin\Widgets\InfoBox;
+use Illuminate\Support\Facades\DB;
+use Nette\Utils\Callback;
 
 class HomeController extends Controller
 {
@@ -18,17 +23,42 @@ class HomeController extends Controller
             ->row(Dashboard::title())
             ->row(function (Row $row) {
 
+                // $row->column(4, function (Column $column) {
+                //     $column->append(Dashboard::environment());
+                // });
+
+                // $row->column(4, function (Column $column) {
+                //     $column->append(Dashboard::extensions());
+                // });
+
+                // $row->column(4, function (Column $column) {
+                //     $column->append(Dashboard::dependencies());
+                // });
                 $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::environment());
+                    $customers=Customer::where('customer_type','1')->count();
+                    $infoBox = new InfoBox('Customers', 'tests', 'aqua', '/admin/customers', $customers);
+                    $column->append($infoBox->render());
+                });
+                $row->column(4, function (Column $column) {
+                    $companies=Customer::where('customer_type','2')->count();
+                    $infoBox = new InfoBox('Companies', 'Companies', 'aqua', '/admin/companies', $companies);
+                    $column->append($infoBox->render());
+
+                });
+                $row->column(4, function (Column $column) {
+                    $income = DB::table("invoices")
+                    ->where('status', '2')
+                    ->where('invoice_date', '>', now()->subDays(30)->endOfDay())
+                    ->sum('total_amount');
+                    // dd($income);
+                    $infoBox = new InfoBox('income', 'tests', 'aqua', '/admin/invoices', $income);
+                    $column->append($infoBox->render());
                 });
 
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::extensions());
-                });
 
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::dependencies());
-                });
             });
     }
 }
+
+
+
