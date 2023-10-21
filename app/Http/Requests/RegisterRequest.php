f6453c74a5dec918443035f6fa4a0bd6e24e2dc0
Nour-Albibi <nour.albibi9@gmail.com>
2023-10-20 23:27:30 -0700

    new updates 20-10-2023

---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Customer;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:255', 'unique:'.Customer::class],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.Customer::class],
+            'password' => ['required', 'confirmed', Password::defaults()],
+        ];
+    }
+}
