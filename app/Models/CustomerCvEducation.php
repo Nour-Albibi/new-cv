090f76850532bcdd0c2fd34cfb3d1905d44c1a6a
Nour-Albibi <nour.albibi9@gmail.com>
2023-10-20 23:26:26 -0700

    new updates 20-10-2023

--- app/Models/CustomerCvEducation.php
+++ app/Models/CustomerCvEducation.php
@@ -9,4 +9,6 @@ class CustomerCvEducation extends Model
 {
     use HasFactory;
     protected $table="customer_cv_educations";
+    protected $fillable=['customer_cv_id','institution_name_en','institution_name_ar','city_ar','city_en','qualification_id',
+        'field_study_ar','field_study_en','honours_ar','honours_en','start_date','end_date','current'];
 }
